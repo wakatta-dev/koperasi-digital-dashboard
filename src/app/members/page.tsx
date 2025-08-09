bf1f78c14cf81cea62295102ee5bb60c5b851fd1
faizalfakhri0001 <faizalfakhri0001@gmail.com>
2025-08-10 01:27:52 +0700

    fix: bug

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,13 +1,17 @@
-import { fetchMembers, Member } from '@/actions/members';
+/** @format */
+
+import { fetchMembers, Member } from "@/actions/members";
 
 export default async function Members() {
   const members: Member[] = await fetchMembers();
+
+  console.log(`Fetched members:`, members);
 
   return (
     <main className="p-8">
       <h1 className="text-2xl font-bold mb-4">Daftar Anggota</h1>
       <ul>
-        {members.map((m) => (
+        {members?.map((m) => (
           <li key={m.id}>{m.name}</li>
         ))}
       </ul>
