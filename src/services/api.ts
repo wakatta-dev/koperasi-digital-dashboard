8be27e789dc13800d73968c2fd91084a37119307
faizalfakhri0001 <faizalfakhri0001@gmail.com>
2025-08-09 23:53:42 +0700

    add: api and service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,14 @@
-import axios from 'axios';
+/** @format */
+
+import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((config) => {
-  if (typeof window !== 'undefined') {
-    const token = localStorage.getItem('accessToken');
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("accessToken");
     if (token) config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
@@ -16,7 +18,7 @@ api.interceptors.response.use(
   (res) => res,
   (err) => {
     if (err.response?.status === 401) {
-      localStorage.removeItem('accessToken');
+      localStorage.removeItem("accessToken");
       // Optional: redirect to login page
     }
     return Promise.reject(err);
