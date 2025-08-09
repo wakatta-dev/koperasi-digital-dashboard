8011fc12255fcb4d7f8386e5b3872802ac620b7c
faizalfakhri0001 <64732731+faizalfakhri0001@users.noreply.github.com>
2025-08-10 01:39:19 +0700

    feat: integrate next-auth for login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import SessionProviderWrapper from '@/components/session-provider';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
     </html>
   );
