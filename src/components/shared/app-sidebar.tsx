33e6fcbfdbcf885028d51b12d5a4999d00bdea97
faizalfakhri0001 <faizalfakhri0001@gmail.com>
2025-08-10 10:36:10 +0700

    fix: collapsible to icon

--- src/components/shared/app-sidebar.tsx
+++ src/components/shared/app-sidebar.tsx
@@ -53,7 +53,7 @@ const data = {
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="offcanvas" {...props}>
+    <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
