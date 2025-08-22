84acf51cf728409a4c23cac78f80923978402fa5
faizalfakhri0001 <64732731+faizalfakhri0001@users.noreply.github.com>
2025-08-22 20:26:01 +0700

    refactor: prevent NEXTAUTH_SECRET from client exposure

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL_INTERNAL,
   },
 };
